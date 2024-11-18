17281b349a6405aab863f3cbb76fc9b1f6d2bb16
Devy <izabela.marcinkowska@outlook.com>
2024-11-18 11:46:48 +0100

    first commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Izabela Marcinkowska on 2024-11-18.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
