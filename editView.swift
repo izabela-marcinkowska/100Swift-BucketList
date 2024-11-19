98091871167b0774dbe917d993f8fa445ce1469c
Devy <izabela.marcinkowska@outlook.com>
2024-11-19 14:58:32 +0100

    feat: Show nearby places with wikipedia

--- editView.swift
+++ editView.swift
@@ -31,6 +31,25 @@ struct editView: View {
     @State private var loadingState = LoadingState.loading
     @State private var pages = [Page]()
     
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        guard let url = URL(string:urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            let items = try JSONDecoder().decode(Result.self, from: data)
+            
+            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            loadingState = .loaded
+        } catch {
+            loadingState = .failed
+        }
+    }
+    
     var body: some View {
         NavigationStack {
             Form {
@@ -65,6 +84,9 @@ struct editView: View {
                     onSave(newLocation)
                     dismiss()
                 }
+            }
+            .task {
+                await fetchNearbyPlaces()
             }
         }
     }
